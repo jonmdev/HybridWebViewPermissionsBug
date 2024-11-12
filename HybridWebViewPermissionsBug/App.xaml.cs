52c0cb2bbbe880190435ccedfdb8ec1dce736418
jonmdev <43948450+jonmdev@users.noreply.github.com>
2024-11-11 20:48:14 -0500

    Initial commit

---
+++ HybridWebViewPermissionsBug/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Java.Lang;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using System.Diagnostics;
+
+
+namespace HybridWebViewPermissionsBug
+{
+    public partial class App : Application
+    {
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            ContentPage mainPage = new();
+            HybridWebView webView = new();
+            mainPage.Content = webView;
+#if ANDROID
+
+            webView.HandlerChanged += delegate {
+                Debug.Assert(webView.Handler != null, "Value must not be null.");
+                var platformView = webView.ToPlatform(webView.Handler.MauiContext);
+                Debug.WriteLine("PLATFORM VIEW " + platformView.GetType());//Microsoft.Maui.Platform.MauiHybridWebView
+                Debug.WriteLine("HANDLER PLATFORM VIEW " + webView.Handler.PlatformView.GetType()); //Microsoft.Maui.Platform.MauiHybridWebView
+                Debug.WriteLine("HANDLER " + webView.Handler.GetType());//Microsoft.Maui.Handlers.HybridWebViewHandler
+                var webViewHandler = (IWebViewHandler)webView.Handler;
+                Debug.Assert(webViewHandler != null, "Value must not be null.");
+                var chromeClient = new MauiAppWebViewHandlers.Platforms.Android.MyWebChromeClient(webViewHandler);
+                webViewHandler.PlatformView.SetWebChromeClient(chromeClient);
+            };
+
+
+#endif
+            return new Window(mainPage);
+        }
+    }
+}
---
+++ HybridWebViewPermissionsBug/ChromeClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//===COPY AND PASTE FROM: https://learn.microsoft.com/en-us/dotnet/maui/user-interface/controls/webview?view=net-maui-8.0&pivots=devices-android
+#if ANDROID
+using Android.Webkit;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace MauiAppWebViewHandlers.Platforms.Android;
+
+internal class MyWebChromeClient : MauiWebChromeClient {
+    public MyWebChromeClient(IWebViewHandler handler) : base(handler) {
+
+    }
+
+    public override void OnPermissionRequest(PermissionRequest request) {
+        // Process each request
+        foreach (var resource in request.GetResources()) {
+            // Check if the web page is requesting permission to the camera
+            if (resource.Equals(PermissionRequest.ResourceVideoCapture, StringComparison.OrdinalIgnoreCase)) {
+                // Get the status of the .NET MAUI app's access to the camera
+                PermissionStatus status = Permissions.CheckStatusAsync<Permissions.Camera>().Result;
+
+                // Deny the web page's request if the app's access to the camera is not "Granted"
+                if (status != PermissionStatus.Granted)
+                    request.Deny();
+                else
+                    request.Grant(request.GetResources());
+
+                return;
+            }
+        }
+
+        base.OnPermissionRequest(request);
+    }
+}
+#endif
+//===================
